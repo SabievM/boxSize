f9d9f61f9fed4e90d4980e3032a6ae970154bc00
SabievM <msabieff@gmail.com>
2025-02-28 17:34:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -25,4 +25,4 @@ def process_numbers():
     return jsonify({"success": True, "sent": result})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
